883f71767a69ebeeb4aaf726a4b77ecd04157f68
elaine.ma <576646388@qq.com>
2016-07-01 11:22:36 +0800

    MANA_HTML
    源码

---
+++ replacement.js
@@ -0,0 +1,70 @@
+module.exports = {
+    AD_ID: '16543',
+    TITLE: 'Wynk Music: MP3 & Hindi songs',
+    ICON: 'http://images.apple.com/autopush/cn/itunes/charts/paid-apps/images/2016/3/16ef147255d45b727d9535dd2e562d958b5ca6be1281e17a8ded6d49a7369e9a.jpg',
+    PICTURE: '/assets/angry-birds.jpg',
+     IMG_320_167: '/assets/angry-birds.jpg',
+    //IMG_320_167: '/assets/test.png',
+    DESC: 'Simple and smooth interface makes shareing files easy than ever. Simple and smooth interface makes shareing files easy than ever. Simple and smooth interface makes shareing files easy than ever.',
+    RATING: '1.5',
+    IS_SHELTER: 1,
+    DOWNLOAD: 1000000,
+    INSTALLS: '50,000,000 +',
+    RATING_COUNTS: 122345890,
+    img1: '/assets/images_1/img1.png',
+    img2: '/assets/images_1/img2.png',
+    img3: '/assets/images_1/img3.png',
+    RESULT: {
+        "ad_list" : [
+            {
+                "ad_id" : 16543,  
+                "icon" :"/assets/appwall/icon1.jpg",
+                "title" : "微信", 
+                "is_shelter" : 1,
+                "rating" : "4.5",  
+                "rating_counts" : "123",
+                "download_num" : 10000000,
+                "description" : " Power Clean is a small, fast, and totally free Android device cleaner and booster featuring a junk cleaner, memory booster, device optimizer, app manager, app locker and privacy protection.", 
+                "images" : [
+                    {
+                        "url":" http://d3bascu0306/108f6a_320x167.png",
+                        "width":320,
+                        "height":167,
+                    } ,
+                    {
+                        "url":" http://d3baa_320x167.png",
+                        "width":320,
+                        "height":167,
+                    }
+                ] 
+            },
+            {
+                "ad_id" : 16543,  
+                "icon" :"/assets/appwall/icon2.jpg",
+                "is_shelter" : 0,
+                "rating" : "3.2", 
+                "rating_counts" : "3452342",
+                "title" : "NAROTO NAROTO NAROTO NAROTO NAROTO NAROTO ",   
+                "download_num" : 10000000,
+                "description" : " Power Clean is a small, fast, and totally free Android device cleaner and booster featuring a junk cleaner, memory booster, device optimizer, app manager, app locker and privacy protection.", 
+                "images" : [
+                    {
+                        "url":" http://d3bascu0306/108f6a_320x167.png",
+                        "width":320,
+                        "height":167,
+                    } ,
+                    {
+                        "url":" http://d3baa_320x167.png",
+                        "width":320,
+                        "height":167,
+                    }
+                ] 
+            }
+        ] 
+    },
+    //以下模拟数据为实时API回传事件用到的
+    IMPRESSION_TRACKING_URL:"https://commondatastorage.googleapis.com/support-kms-prod/769C0ED5533AD3A92CC97E6F22F4D8112C92",//广告展示追踪地址
+    CLICK_TRACKING_URL:"http://template.dev.dy/text.json?_=1466649783275", //广告点击追踪地址
+    CLICK_THROUGH_URL:  "http://dsp.dev.dy"//广告跳转地址
+
+}
